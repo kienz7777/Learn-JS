271bc6d4189ee5fb4dce1dcf8a2fd0f764d690c8
kienz7777 <kienvu7777@gmail.com>
2020-12-16 21:29:14 +0700

    string

---
+++ String/indexOf.js
@@ -0,0 +1,11 @@
+var str = "Hello every body today i fell so good";
+
+//indexOf
+console.log(str.indexOf("body")); //12
+//includes
+console.log(str.includes("body")); //true
+//replace
+console.log(str.replace("body","one")); //Hello every one today i fell so good
+//trim  : remove ' ' top and bot
+var st = " hel kdo le enough ";
+console.log(st.trim()); // hel kdo le enough
