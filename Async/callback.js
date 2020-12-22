9c1f252c9d67eee6bc80c815a1f7c2ede447034e
kienz7777 <kienvu7777@gmail.com>
2020-12-22 21:23:15 +0700

    call-back hell

--- Async/callback.js
+++ Async/callback.js
@@ -27,3 +27,48 @@ danhRang(()=>{
         })
     })
 });
+
+// call-back
+const sumTotal = (num1,num2,cb) => {
+    if((num1 + num2) % 2 == 0)
+        return cb({error : false, mess: 'ENC'})
+    return cb({error : true, mess : 'LOCK'})
+}
+
+sumTotal(1,3,(sum)=>{
+    if(sum.error == false){
+        console.log('ERR');
+        console.log(sum);
+    }
+    else{
+        console.log('SUC');
+        console.log(sum);
+    }
+});
+
+
+
+//call-back
+
+let add = (a,b,cb) => {
+    setTimeout(()=>{
+        let err,result;
+        if(typeof a != 'number' || typeof b != 'number'){
+            err = 'Lõi nha!!!'
+            return cb(err,result);
+        }
+        else{
+            result = a + b;
+            return cb(err,result);
+        }
+    },1000);
+}
+
+add(1,'p',(err,result) => {
+    if(err){
+        console.log('Err ' + err);
+    }
+    else{
+        console.log('Result: ' + result);
+    }
+})
---
+++ Async/promise.js
@@ -0,0 +1,14 @@
+// Promise have 2 parameter : resolve = then ,reject = catch
+// Run resolve or reject then stop
+
+// const promise1 = new Promise((resolve,reject) => {
+//     //resolve('hello word');
+//     reject('hi');
+// });
+
+// promise1
+//     .then(res => console.log(res))
+//     .catch(err => console.log(`err: ${err}`))
+
+
+
