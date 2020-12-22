f4739afe77beb47477cc18418c345c8c185dc706
kienz7777 <kienvu7777@gmail.com>
2020-12-22 21:36:30 +0700

    promise

--- Async/promise.js
+++ Async/promise.js
@@ -10,5 +10,18 @@
 //     .then(res => console.log(res))
 //     .catch(err => console.log(`err: ${err}`))
 
+let add = (a,b) => {
+    return new Promise((resolve,reject) => {
+        setTimeout(()=>{
+            if(typeof a != 'number' || typeof b != 'number'){
+                return reject(new Error('Tham số phải là number'))
+            }
+            return resolve(a+b);
+        },1000);
+    })
+}
 
+add(4,"5")
+    .then(res => console.log(res))
+    .catch(err => console.log(err + ''))
 
