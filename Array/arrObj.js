527c21eca3f51e4901c5f2e6274c9d538b0985bc
kienz7777 <kienvu7777@gmail.com>
2020-12-29 21:31:07 +0700

    test

---
+++ Array/arrObj.js
@@ -0,0 +1,14 @@
+const cat ={
+    color : [],
+    weight : []
+}
+
+cat['color'].push('black')
+cat['color'].push('white')
+
+cat['weight'].push(25)
+cat['weight'].push(54)
+
+cat['tits'] = 6
+
+console.log(cat);
