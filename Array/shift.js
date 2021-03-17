a9670544c993aa6acc8477d7075a9ec53de1e7de
kienz7777 <kienvu7777@gmail.com>
2021-03-17 07:42:44 +0700

    arr shift

---
+++ Array/shift.js
@@ -0,0 +1,12 @@
+// shift() will remove the first element of an array and 
+//return that element. After execution, this function will
+//change the size of the affected array.
+
+
+var myFish = ['angel', 'clown', 'mandarin', 'surgeon'];
+
+var shifted = myFish.shift();
+
+console.log(myFish);      // [ 'clown', 'mandarin', 'surgeon' ]
+console.log(shifted);    // angle
+ 
